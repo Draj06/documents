1af1271a2f5de52ff66c86a1463d6b8e56a8fe8e
Albin-Joseph <60734163+albin-joseph-wiznet@users.noreply.github.com>
2020-02-12 23:13:08 +0530

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -42,8 +42,8 @@ const siteConfig = {
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
-    {href: 'https://github.com/Wiznet/documents', label: 'GitHub'},
-    {href: 'https://v2.convertapi.com/convert/web/to/pdf?secret=&download=attachment&url=' + encodeURI(window.location), label: 'PDF'},
+ //   {href: 'https://github.com/Wiznet/documents', label: 'GitHub'},
+ //   {href: 'https://v2.convertapi.com/convert/web/to/pdf?secret=&download=attachment&url=' + encodeURI(window.location), label: 'PDF'},
   ],
 
   // If you have users set above, you add it here:
