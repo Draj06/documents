0e36867d85bae6add651fa43a5e6aab62c1df717
Albin-Joseph <60734163+albin-joseph-wiznet@users.noreply.github.com>
2020-03-03 16:19:19 +0530

    Update Footer.js

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -94,7 +94,7 @@ class Footer extends React.Component {
                 <div
                   className="fb-like"
                   data-href={this.props.config.url}
-                  data-colorscheme="dark"
+                  data-colorscheme="light"
                   data-layout="standard"
                   data-share="true"
                   data-width="225"
