e3658dd4ae6f4286961c1416938d6974f869d3e1
ann jaison <60734085+annmariajaison@users.noreply.github.com>
2020-03-05 13:05:47 +0530

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,7 +38,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'OVERVIEW', label: 'docs'},
+    {doc: 'OVERVIEW', label: 'DOCUMENTATION'},
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
